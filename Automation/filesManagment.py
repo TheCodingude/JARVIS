c7088b33b0c6214039f8ee5464b7c02341346c80
CodingDude <sharpdt06@gmail.com>
2023-04-05 20:20:05 -0400

    added basic html structure

--- Automation/filesManagment.py
+++ Automation/filesManagment.py
@@ -22,8 +22,21 @@ def createNewWebDevProject(name):
 
    os.makedirs(path)
 
-   with open("index.html", "w") as f:
-      f.close()
+   with open(f"{path}\\index.html", "w") as f:
+      f.write("<!DOCTYPE html>\n")
+      f.write('<html lang="en">\n')
+      f.write('<head>\n')
+      f.write('   <meta charset="UTF-8">\n')
+      f.write('   <meta http-equiv="X-UA-Compatible" content="IE=edge">\n')
+      f.write('   <meta name="viewport" content="width=device-width, initial-scale=1.0">\n')
+      f.write('   <title>Document</title>\n')
+      f.write('   <link rel="stylesheet" href="style.css">\n')
+      f.write('   <script defer src="index.js"></script>\n')
+      f.write('</head>\n')
+      f.write('<body>\n')
+      f.write('   <h1>TEST</h1>\n')
+      f.write('</body>\n')
+      f.write('</html>\n')
 
    with open("style.css", "w") as f:
       f.close()
@@ -36,5 +49,5 @@ def openProject(project_path):
    os.system(f"code {project_path}")
 
 
-openProject("C:\\Users\\sharp\\Desktop\\Old-Programming-Language")
+createNewWebDevProject("lol")
 
